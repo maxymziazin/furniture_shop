4293c2700276df1739d7ea68c9afde8eefa2c847
maxym_ziazin <aleks.cauchy.n15@gmail.com>
2021-03-21 14:29:09 +0200

    second commit

--- js/main.js
+++ js/main.js
@@ -1,14 +1,23 @@
 $(function () {
-  let $roomSlider = $("#room-slider");
+  let $furnitureSlider = $("#furniture-slider");
 
-  $roomSlider.slick({
+  $furnitureSlider.slick({
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
     dots: true,
     prevArrow:
-      '<button type="button" class="room-slider__btn slick-prev"><i class="fas fa-chevron-left"></i></button>',
+      '<button type="button" class="furniture-slider__btn slick-prev"><i class="fas fa-chevron-left"></i></button>',
     nextArrow:
-      '<button type="button" class="room-slider__btn slick-next"><i class="fas fa-chevron-right"></i></button>',
+      '<button type="button" class="furniture-slider__btn slick-next"><i class="fas fa-chevron-right"></i></button>',
+  });
+
+  let $roomSlider = $("#room-slider");
+
+  $roomSlider.slick({
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
   });
 });
